3a017eb3b2a7e799f3d0e8b883c2848cf1e54159
Keith Stedman <keithstedman@gmail.com>
2017-05-08 09:20:32 -0700

    building app component

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,8 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <h1>Zoo App</h1>
-  `
+  <div class="container">
+  <h1>Angular2 CLI</h1>
+  <h2>{{firstTask.description}}</h2>
+  </div>`
 })
 
-export class AppComponent {}
+export class AppComponent {
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+
+    firstTask = {
+      description: "Build dope-ass apps"
+    }
+
+}
+
+export class Task {
+  public done: boolean = false;
+  constructor(public description: string){
+    
+  }
+
+}
