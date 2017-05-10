ce6add647707e57aaab5c7c6ea8f6d2d1ca81840
Keith Stedman <keithstedman@gmail.com>
2017-05-10 11:16:35 -0700

    added carsouel

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,7 +5,6 @@ import { Animal } from './animal.model';
   selector: 'app-root',
   template: `
   <div class="container">
-    <h1>Angular2 Cli</h1>
     <animal-list [childAnimalList]="masterAnimalList" (editClickSender)="editName($event)"></animal-list>
     <edit-animal [childSelectedAnimal]="selectedAnimal" (doneButtonClickedSender)="finishedEditing()"></edit-animal>
     <new-animal (newAnimalSender)="addAnimal($event)"></new-animal>
